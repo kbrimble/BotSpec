5ee5a3fc21826618ccb800a0b070e1928f3b17b6
Kristian Brimble <brimble2010@gmail.com>
2016-12-01 11:37:07 +0000

    CardActionAssertion tests passing

--- KBrimble.DirectLineTester/Assertions/Cards/CardActionAssertions.cs
+++ KBrimble.DirectLineTester/Assertions/Cards/CardActionAssertions.cs
@@ -16,47 +16,96 @@ namespace KBrimble.DirectLineTester.Assertions.Cards
 
         public ICardActionAssertions HasTitleMatching(string regex)
         {
-            throw new NotImplementedException();
+            if (regex == null)
+                throw new ArgumentNullException(nameof(regex));
+
+            StringHelpers.TestForMatch(_cardAction.Title, regex, CreateEx(nameof(_cardAction.Title), regex));
+
+            return this;
         }
 
         public ICardActionAssertions HasTitleMatching(string regex, string groupMatchingRegex, out IList<string> groupMatches)
         {
-            throw new NotImplementedException();
+            if (regex == null)
+                throw new ArgumentNullException(nameof(regex));
+            if (groupMatchingRegex == null)
+                throw new ArgumentNullException(nameof(groupMatchingRegex));
+
+            groupMatches = StringHelpers.TestForMatchAndReturnGroups(_cardAction.Title, regex, groupMatchingRegex, CreateEx(nameof(_cardAction.Title), regex));
+
+            return this;
         }
 
         public ICardActionAssertions HasValueMatching(string regex)
         {
-            throw new NotImplementedException();
+            if (regex == null)
+                throw new ArgumentNullException(nameof(regex));
+
+            StringHelpers.TestForMatch(_cardAction.Value, regex, CreateEx(nameof(_cardAction.Value), regex));
+
+            return this;
         }
 
         public ICardActionAssertions HasValueMatching(string regex, string groupMatchingRegex, out IList<string> groupMatches)
         {
-            throw new NotImplementedException();
+            if (regex == null)
+                throw new ArgumentNullException(nameof(regex));
+            if (groupMatchingRegex == null)
+                throw new ArgumentNullException(nameof(groupMatchingRegex));
+
+            groupMatches = StringHelpers.TestForMatchAndReturnGroups(_cardAction.Value, regex, groupMatchingRegex, CreateEx(nameof(_cardAction.Value), regex));
+
+            return this;
         }
 
         public ICardActionAssertions HasTypeMatching(string regex)
         {
-            throw new NotImplementedException();
+            if (regex == null)
+                throw new ArgumentNullException(nameof(regex));
+
+            StringHelpers.TestForMatch(_cardAction.Type, regex, CreateEx(nameof(_cardAction.Type), regex));
+
+            return this;
         }
 
         public ICardActionAssertions HasTypeMatching(string regex, string groupMatchingRegex, out IList<string> groupMatches)
         {
-            throw new NotImplementedException();
+            if (regex == null)
+                throw new ArgumentNullException(nameof(regex));
+            if (groupMatchingRegex == null)
+                throw new ArgumentNullException(nameof(groupMatchingRegex));
+
+            groupMatches = StringHelpers.TestForMatchAndReturnGroups(_cardAction.Type, regex, groupMatchingRegex, CreateEx(nameof(_cardAction.Type), regex));
+
+            return this;
         }
 
         public ICardActionAssertions HasImageMatching(string regex)
         {
-            throw new NotImplementedException();
+            if (regex == null)
+                throw new ArgumentNullException(nameof(regex));
+
+            StringHelpers.TestForMatch(_cardAction.Image, regex, CreateEx(nameof(_cardAction.Image), regex));
+
+            return this;
         }
 
         public ICardActionAssertions HasImageMatching(string regex, string groupMatchingRegex, out IList<string> groupMatches)
         {
-            throw new NotImplementedException();
+            if (regex == null)
+                throw new ArgumentNullException(nameof(regex));
+            if (groupMatchingRegex == null)
+                throw new ArgumentNullException(nameof(groupMatchingRegex));
+
+            groupMatches = StringHelpers.TestForMatchAndReturnGroups(_cardAction.Image, regex, groupMatchingRegex, CreateEx(nameof(_cardAction.Image), regex));
+
+            return this;
         }
 
         public CardActionAssertionFailedException CreateEx(string testedProperty, string regex)
         {
-            throw new NotImplementedException();
+            var message = $"Expected card action to have property {testedProperty} matching {regex} but it did not.";
+            return new CardActionAssertionFailedException(message);
         }
     }
 
